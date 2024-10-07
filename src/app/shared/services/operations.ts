aa403d347c018ef67ed081c216e5093a2d09e170
Juan Canelon <liscanojac@gmail.com>
2024-10-07 20:59:06 -0300

    feat: operations

--- src/app/shared/services/operations.ts
+++ src/app/shared/services/operations.ts
@@ -1,12 +1,89 @@
+import { BaseOperand, Operation, Result } from "./models/calculator.model";
 import { OperationFunctions } from "./models/operations.model";
+
+export const initialOperand: BaseOperand = {
+  value: 0,
+  label: '',
+  decimal: false
+};
+
+export const initialOperation: Operation = {
+  eval: undefined,
+  selected: false,
+  label: ''
+};
+
+export const initialResult: Result = {
+  ...initialOperand,
+  isError: false,
+  errorCode: "-1"
+};
 
 export const operations: OperationFunctions = {
   add: (a, b) => {
-    let c = a + b;
-    return c;
+    if (b === undefined) return getResult(a);
+    const c = a + b;
+
+    return getResult(c);
   },
-  substract: (a, b) => a,
-  multiply: (a, b) => a,
-  divide: (a, b) => a,
-  percent: (a, b) => a,
+  substract: (a, b) => {
+    if (b === undefined) return getResult(a);
+    const c = a - b;
+
+    return getResult(c);
+  },
+  multiply: (a, b) => {
+    if (b === undefined) return getResult(a);
+    const c = a * b;
+
+    return getResult(c);
+  },
+  divide: (a, b) => {
+
+    if(b === 0) {
+      return {
+        value: 0,
+        isError: true,
+        label: '',
+        decimal: false,
+        errorCode: "0"
+      }
+    }
+    if (!b) return getResult(a);
+    const c = a / b;
+
+    return getResult(c);
+  },
+  percent: (a, b) => {
+    if (b === 0) return getResult(b);
+    if (!b) return operations.divide(a, 100);
+    if (b === 100) return getResult(a);
+    if (a === 100) return getResult(b);
+
+    const c = formatDecimals(b * (a / 100))
+
+    return getResult(c);
+  },
+}
+
+function hasDecimals(num: number): boolean {
+  return num % 1 != 0;
+}
+
+function formatDecimals(a: number): number {
+  const b = a.toFixed(2);
+
+  return parseFloat(b);
+}
+
+function getResult(a: number): Result {
+
+  const resultValue = parseFloat(a.toFixed(14));
+  return {
+    value: resultValue,
+    isError: false,
+    label: resultValue.toString(),
+    decimal: hasDecimals(a),
+    errorCode: "-1"
+  };
 }
