bf4a108c384519d5a5e9e82dfef0c4ddeeda50cc
Juan Canelon <liscanojac@gmail.com>
2024-10-07 21:02:33 -0300

    feat: buttons

---
+++ src/app/shared/services/buttons.ts
@@ -0,0 +1,229 @@
+import { Button } from "./models/button.model";
+
+export const buttons: Array<Button> = [
+    {
+      id: 0,
+      operand: 'numeric',
+      label: '0',
+      value: 0
+    },
+    {
+      id: 1,
+      operand: 'numeric',
+      label: '1',
+      value: 1
+    },
+    {
+      id: 2,
+      operand: 'numeric',
+      label: '2',
+      value: 2
+    },
+    {
+      id: 3,
+      operand: 'numeric',
+      label: '3',
+      value: 3
+    },
+    {
+      id: 4,
+      operand: 'numeric',
+      label: '4',
+      value: 4
+    },
+    {
+      id: 5,
+      operand: 'numeric',
+      label: '5',
+      value: 5
+    },
+    {
+      id: 6,
+      operand: 'numeric',
+      label: '6',
+      value: 6
+    },
+    {
+      id: 7,
+      operand: 'numeric',
+      label: '7',
+      value: 7
+    },
+    {
+      id: 8,
+      operand: 'numeric',
+      label: '8',
+      value: 8
+    },
+    {
+      id: 9,
+      operand: 'numeric',
+      label: '9',
+      value: 9
+    },
+    {
+      id: 10,
+      operand: 'decimal',
+      label: '.',
+      value: 0
+    },
+    {
+      id: 11,
+      operand: 'equal',
+      label: '=',
+      value: 0
+    },
+    {
+      id: 12,
+      operand: 'operation',
+      label: '+',
+      value: 'add'
+    },
+    {
+      id: 13,
+      operand: 'operation',
+      label: '-',
+      value: 'substract'
+    },
+    {
+      id: 14,
+      operand: 'operation',
+      label: '*',
+      value: 'multiply'
+    },
+    {
+      id: 15,
+      operand: 'operation',
+      label: '/',
+      value: 'divide'
+    },
+    {
+      id: 16,
+      operand: 'operation',
+      label: '%',
+      value: 'percent'
+    },
+    {
+      id: 17,
+      operand: 'deletion',
+      label: 'erase',
+      value: 'erase'
+    },
+    {
+      id: 18,
+      operand: 'deletion',
+      label: 'reset',
+      value: 'reset'
+    }
+  ];
+
+  export const buttonsObj = {
+    one: {
+      id: 1,
+      operand: 'numeric',
+      label: '1',
+      value: 1
+    },
+    two: {
+      id: 2,
+      operand: 'numeric',
+      label: '2',
+      value: 2
+    },
+    three: {
+      id: 3,
+      operand: 'numeric',
+      label: '3',
+      value: 3
+    },
+    four: {
+      id: 4,
+      operand: 'numeric',
+      label: '4',
+      value: 4
+    },
+    five: {
+      id: 5,
+      operand: 'numeric',
+      label: '5',
+      value: 5
+    },
+    six: {
+      id: 6,
+      operand: 'numeric',
+      label: '6',
+      value: 6
+    },
+    seven: {
+      id: 7,
+      operand: 'numeric',
+      label: '7',
+      value: 7
+    },
+    eight: {
+      id: 8,
+      operand: 'numeric',
+      label: '8',
+      value: 8
+    },
+    nine: {
+      id: 9,
+      operand: 'numeric',
+      label: '9',
+      value: 9
+    },
+    ten: {
+      id: 10,
+      operand: 'numeric',
+      label: '0',
+      value: 0
+    },
+    decimal: {
+      id: 11,
+      operand: 'numeric',
+      label: '.',
+      value: 0
+    },
+    add: {
+      id: 12,
+      operand: 'operation',
+      label: '+',
+      value: 'add'
+    },
+    substract: {
+      id: 13,
+      operand: 'operation',
+      label: '-',
+      value: 'substract'
+    },
+    multiply: {
+      id: 14,
+      operand: 'operation',
+      label: '*',
+      value: 'multiply'
+    },
+    divide: {
+      id: 15,
+      operand: 'operation',
+      label: '/',
+      value: 'divide'
+    },
+    percent: {
+      id: 16,
+      operand: 'operation',
+      label: '%',
+      value: 'percent'
+    },
+    erase: {
+      id: 17,
+      operand: 'deletion',
+      label: 'erase',
+      value: 'erase'
+    },
+    delete: {
+      id: 18,
+      operand: 'deletion',
+      label: 'reset',
+      value: 'reset'
+    }
+  }
